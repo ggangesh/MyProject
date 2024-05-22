f1635347a8ea7296380ba526caf0f42233ede30a
aishwaryaB3695 <aishwaryabolegave@gmail.com>
2024-05-22 11:29:57 +0530

    My first project

---
+++ MyFirstProject/src/com/aishwarya/firstProject/Demo1.java
@@ -0,0 +1,14 @@
+package com.aishwarya.firstProject;
+
+public class Demo1 {
+
+	public void m1() {
+	    System.out.println("this is m1 method");
+	}
+	public static void main(String[] args) {
+		Demo1 d=new Demo1();
+		d.m1();
+
+	}
+
+}
